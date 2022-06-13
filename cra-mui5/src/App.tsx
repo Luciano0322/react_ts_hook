446d16c5f11748586ebc160b4833c2d0854ec936
Luciano Lee <luciano.lee@webcomm.com.tw>
2022-06-13 07:00:54 +0000

    使用route config的方式來整理路由

--- cra-mui5/src/App.tsx
+++ cra-mui5/src/App.tsx
@@ -1,80 +1,35 @@
 import { Container, createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, useRoutes } from 'react-router-dom';
-import useToggle from './hooks/useToggle';
-import Home from './pages/Home';
-import Layout from './pages/Layout';
-import MuiButtonList from './pages/MuiButtonList';
-import MuiCheckBoxList from './pages/MuiCheckboxList';
-import MuiRadioList from './pages/MuiRadioList';
-import MuiSelectList from './pages/MuiSelectList';
-import MuiSwitchList from './pages/MuiSwitchList';
-import MuiTabsList from './pages/MuiTabsList';
-import MuiTextFields from './pages/MuiTextFieldList';
-import MuiTextList from './pages/MuiTextList';
-import PageError from './pages/PageError';
+// import useToggle from './hooks/useToggle';
+// import Home from './pages/Home';
+// import Layout from './pages/Layout';
+// import MuiButtonList from './pages/MuiButtonList';
+// import MuiCheckBoxList from './pages/MuiCheckboxList';
+// import MuiRadioList from './pages/MuiRadioList';
+// import MuiSelectList from './pages/MuiSelectList';
+// import MuiSwitchList from './pages/MuiSwitchList';
+// import MuiTabsList from './pages/MuiTabsList';
+// import MuiTextFields from './pages/MuiTextFieldList';
+// import MuiTextList from './pages/MuiTextList';
+// import PageError from './pages/PageError';
 import { withBWTheme } from './styles/mainTheme';
 import { useSelector } from 'react-redux';
 import { RootState } from './lib/store';
+import RoutesConfig from './pages/RoutesConfig';
 
 
 function App() {
   // 這裡應該用gloabel state來處理
   // const [dark, setDark] = useToggle(true)
   const { dark } = useSelector((state: RootState) => state.darkMod) 
-  
   const bwModeTheme = createTheme(withBWTheme(dark ? 'dark': 'light'));
-  // const routeConfig = useRoutes([
-  //   { 
-  //     path: "/", 
-  //     element: <Layout dark={dark} darkFn={setDark}/>, 
-  //     children: [
-  //       { path: '/', element: <Home /> },
-  //     ] 
-  //   },
-  //   {
-  //     path: "muiText",
-  //     element: <MuiTextList/>,
-  //   },
-  //   {
-  //     path: "muiBtns",
-  //     element: <MuiButtonList/>,
-  //   },
-  //   {
-  //     path: "muiTextFields",
-  //     element: <MuiTextFields/>,
-  //   },
-  //   {
-  //     path: "muiSelects",
-  //     element: <MuiSelectList/>,
-  //   },
-  //   {
-  //     path: "muiRadios",
-  //     element: <MuiRadioList/>,
-  //   },
-  //   {
-  //     path: "muiCheckboxs",
-  //     element: <MuiCheckBoxList/>,
-  //   },
-  //   {
-  //     path: "muiSwitchs",
-  //     element: <MuiSwitchList/>,
-  //   },
-  //   {
-  //     path: "muiTabs",
-  //     element: <MuiTabsList/>,
-  //   },
-  //   {
-  //     path: "*",
-  //     element: <PageError/>,
-  //   },
-  // ])
   return (
     <Router>
       <ThemeProvider theme={bwModeTheme}>
         <CssBaseline/>
         <Container maxWidth="lg">
-          <Routes>
+          {/* <Routes>
             <Route path="/" element={<Layout/>}>
               <Route index element={<Home/>} />
               <Route path="muiText" element={<MuiTextList/>} />
@@ -89,7 +44,8 @@ function App() {
               </Route>
               <Route path="*" element={<PageError />} />
             </Route>
-          </Routes>
+          </Routes> */}
+          <RoutesConfig/>
         </Container>
       </ThemeProvider>
     </Router>
--- cra-mui5/src/pages/RoutesConfig.tsx
+++ cra-mui5/src/pages/RoutesConfig.tsx
@@ -1,9 +1,64 @@
 import React from 'react'
+import { useRoutes } from 'react-router-dom'
+import Home from './Home';
+import Layout from './Layout';
+import MuiButtonList from './MuiButtonList';
+import MuiCheckBoxList from './MuiCheckboxList';
+import MuiRadioList from './MuiRadioList';
+import MuiSelectList from './MuiSelectList';
+import MuiSwitchList from './MuiSwitchList';
+import MuiTabsList from './MuiTabsList';
+import MuiTextFields from './MuiTextFieldList';
+import MuiTextList from './MuiTextList';
+import PageError from './PageError';
 
 const RoutesConfig = () => {
-  return (
-    <div>RoutesConfig</div>
-  )
+  const routeConfig = useRoutes([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { path: '/', element: <Home /> },
+        {
+          path: "muiText",
+          element: <MuiTextList />,
+        },
+        {
+          path: "muiBtns",
+          element: <MuiButtonList />,
+        },
+        {
+          path: "muiTextFields",
+          element: <MuiTextFields />,
+        },
+        {
+          path: "muiSelects",
+          element: <MuiSelectList />,
+        },
+        {
+          path: "muiRadios",
+          element: <MuiRadioList />,
+        },
+        {
+          path: "muiCheckboxs",
+          element: <MuiCheckBoxList />,
+        },
+        {
+          path: "muiSwitchs",
+          element: <MuiSwitchList />,
+        },
+        {
+          path: "muiTabs",
+          element: <MuiTabsList />,
+        },
+      ]
+    },
+    {
+      path: "*",
+      element: <PageError />,
+    },
+  ])
+  return routeConfig
 }
 
 export default RoutesConfig
