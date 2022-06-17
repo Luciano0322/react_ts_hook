b59a5ec8624757689a95050405f08d925025df4f
Luciano Lee <luciano.lee@webcomm.com.tw>
2022-06-17 10:46:05 +0000

    vite Mui

---
+++ vitemui/src/App.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react"
+import { CssBaseline, Button } from '@mui/material'
+
+const App: FC = () => {
+
+  return (
+    <>
+      <CssBaseline/>
+      <Button variant="contained" color="primary">test</Button>
+    </>
+  )
+}
+
+export default App
